e40b9df38bc7fa38ec4c4b72f85a33dad2cf9a3c
Ekeudj <ekeujunior5@gmail.com>
2026-02-14 03:14:12 +0300

    File upload sucess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from database import engine, create_db_and_tables, SessionDep
 from models import AudioProject
+import shutil
+import os
 
 app = FastAPI()
 
@@ -12,11 +14,18 @@ def on_startup():
 def root():
     return{"message":"We're live!!"}
 
-#temporary test to make sure supabase is receving
-@app.post('/test_db')
-def test_db(project_title: str,session:SessionDep):
-    new_project = AudioProject(title=project_title)
+#route for actual audio upload
+@app.post('/upload')
+async def upload_audio(session: SessionDep, file: UploadFile = File(...)):
+    upload_dir = "uploads"
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+
+    file_location = f"{upload_dir}/{file.filename}"
+    with open(file_location, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)
+
+    new_project = AudioProject(title=file.filename, file_path=file_location)
     session.add(new_project)
     session.commit()
     session.refresh(new_project)
-    return {"message": f"Project '{new_project.title}' created with ID {new_project.id}."}
--- models.py
+++ models.py
@@ -6,5 +6,7 @@ class AudioProject(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     # uploaded, cleaning, transcribing, completed we'll also use it for tarcking later on
+    file_path: Optional[str] = None
+    
     status: str = Field(default="uploaded") 
     created_at: datetime = Field(default_factory=datetime.utcnow)
